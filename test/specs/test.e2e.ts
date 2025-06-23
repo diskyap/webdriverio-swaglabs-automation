403b914f83273c64f1772469cc489e9958f88416
diskyap <diskyadit03@gmail.com>
2025-06-23 20:57:30 +0700

    feat: Initial setup of WebdriverIO project with Allure reporter

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,10 @@
+//import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('standard_user', 'secret_sauce!')
+    })
+})
